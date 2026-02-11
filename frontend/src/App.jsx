f5612c7879a4c715f7ad5f521deacb340cfd8f0d
abishekvarma <abishek@entrolabs.com>
2026-02-12 03:44:33 +0530

    feat(app): configure routes for new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,9 @@ import Loader from "./components/Loader";
 
 const Home = lazy(() => import("./pages/Home"));
 const UploadPage = lazy(() => import("./pages/UploadPage"));
+const DocumentsPage = lazy(() => import("./pages/DocumentsPage"));
+const AskPage = lazy(() => import("./pages/AskPage"));
+const StatusPage = lazy(() => import("./pages/StatusPage"));
 
 function App() {
   return (
@@ -17,6 +20,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/upload" element={<UploadPage />} />
+            <Route path="/documents" element={<DocumentsPage />} />
+            <Route path="/ask" element={<AskPage />} />
+            <Route path="/status" element={<StatusPage />} />
           </Routes>
         </Suspense>
       </BrowserRouter>
